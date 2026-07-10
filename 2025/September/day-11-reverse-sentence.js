0b83855b68f706e6758d73fca5fe06e1ff25906b
Hari Babu <hariprasaanth1998@gmail.com>
2026-07-10 08:33:57 +0530

    reverse sentence solution

--- 2025/September/day-11-reverse-sentence.js
+++ 2025/September/day-11-reverse-sentence.js
@@ -4,7 +4,17 @@
 // The returned string should only have one space between words.
 
 function reverseSentence(sentence) {
-  return sentence;
+  let resultArray = [];
+  const sentenceArray = String(sentence).split(" ");
+  for(let i=sentenceArray.length-1;i>=0;i--){
+    if(sentenceArray[i]!==''){
+      resultArray.push(sentenceArray[i])
+    }
+  }
+  return resultArray.join(" ");
 }
 
-console.log(reverseSentence("world hello"));
+console.log(reverseSentence("world hello")); //return "hello world"
+console.log(reverseSentence("push commit git")); //return "git commit push"
+console.log(reverseSentence("npm  install  sudo")); //return sudo install npm
+console.log(reverseSentence("import    default   function  export")); //return export function default import
