0754a1e02acb107ada0f6bc593fb608c5ba6f2da
Hari Prasaanth <hariprasaanth1998@gmail.com>
2026-07-10 11:22:30 +0530

    roommates solution

---
+++ 2026/June/day-9-roommates.js
@@ -0,0 +1,76 @@
+// Given an array of people and their roommate group, return the room assignments for a hotel stay using the following rules:
+
+// Each person has a name and a group property:
+/*
+[
+  { "name": "Alice", "group": "A" },
+  { "name": "Bob", "group": "B" },
+  { "name": "Carol", "group": "A" }
+]
+  */
+// People can only share a room with someone from the same group and are paired in the order they are given.
+// Return an array of strings with names separated by " and " for a shared room, and just the name for a solo room. Names must appear in the order they were paired. For the example above, return ["Alice and Carol", "Bob"].
+
+function getMates(people, group) {
+  const groupOrg = people
+    .filter((ppl) => ppl.group === group)
+    .map((ppl) => ppl.name);
+  return groupOrg;
+}
+
+function getRoommates(people) {
+  const uniqueGroup = [...new Set(people.map((ppl) => ppl.group))];
+  const getOrgGroup = uniqueGroup.map((uni) => getMates(people, uni));
+  let resultArray = [];
+  for (let i = 0; i < getOrgGroup.length; i++) {
+    for (let j = 0; j < getOrgGroup[i].length; j += 2) {
+      resultArray.push(getOrgGroup[i].slice(j, j + 2).join(" and "));
+    }
+  }
+  return resultArray;
+}
+
+console.log(
+  getRoommates([
+    { name: "Alice", group: "A" },
+    { name: "Bob", group: "B" },
+    { name: "Carol", group: "A" },
+  ]),
+); //return ["Alice and Carol", "Bob"]
+console.log(
+  getRoommates([
+    { name: "John", group: "C" },
+    { name: "Julia", group: "C" },
+    { name: "Jim", group: "C" },
+  ]),
+); //return ["John and Julia", "Jim"]
+console.log(
+  getRoommates([
+    { name: "Adam", group: "D" },
+    { name: "Abraham", group: "E" },
+    { name: "Austin", group: "E" },
+    { name: "Augustus", group: "D" },
+    { name: "Angelica", group: "D" },
+    { name: "Aaron", group: "E" },
+  ]),
+); //return ["Adam and Augustus", "Angelica", "Abraham and Austin", "Aaron"]
+console.log(
+  getRoommates([
+    { name: "Frank", group: "A" },
+    { name: "Emitt", group: "B" },
+    { name: "Daria", group: "F" },
+    { name: "Charles", group: "D" },
+    { name: "Bailey", group: "A" },
+    { name: "Albert", group: "F" },
+  ]),
+); //return ["Frank and Bailey", "Emitt", "Daria and Albert", "Charles"]
+console.log(
+  getRoommates([
+    { name: "Kevin", group: "A" },
+    { name: "Yuri", group: "A" },
+    { name: "Hugo", group: "B" },
+    { name: "Violet", group: "A" },
+    { name: "Brett", group: "A" },
+    { name: "Wayne", group: "B" },
+  ]),
+); //return ["Kevin and Yuri", "Violet and Brett", "Hugo and Wayne"]
